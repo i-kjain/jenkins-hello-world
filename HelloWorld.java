ba250b864bc58c032eb36b0ffb0918874aaec8f9
i-kjain <155384457+i-kjain@users.noreply.github.com>
2026-03-19 19:05:29 +0530

    Fix syntax error in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,1 @@
-public class HelloWorld { public static void main(String[] args) { System.out.println("Hello, Jenkins CI/CD!") } }
+public class HelloWorld { public static void main(String[] args) { System.out.println("Hello, Jenkins CI/CD!"); } }
